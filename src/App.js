c8579d9a30e00bbdfa17cf8388f167e41e09f03d
Liam Desmond <liamjdesmond84@gmail.com>
2021-12-19 20:53:22 -0600

    removing excess

--- src/App.js
+++ src/App.js
@@ -1,16 +1,11 @@
-import React, { useState, useRef } from "react";
+import React, { useState } from "react";
 import axios from "axios";
-import {
-    GoogleMap,
-    useLoadScript,
-    Marker,
-    InfoWindow,
-} from "@react-google-maps/api";
+import { GoogleMap, useLoadScript } from "@react-google-maps/api";
 import "./App.css";
 
 const mapContainerStyle = {
     width: "70vw",
-    height: "50vh",
+    height: "70vh",
 };
 
 function App() {
@@ -20,32 +15,11 @@ function App() {
     const [lat, setLat] = useState(32.715736);
     const [lng, setLng] = useState(-117.161087);
     const [libraries] = useState(["places"]);
-    const [markers, setMarkers] = useState([]);
-    const [selected, setSelected] = useState(null);
 
     const center = {
         lat: parseFloat(lat),
         lng: parseFloat(lng),
     };
-
-    const onMapClick = React.useCallback(
-        (e) =>
-            setMarkers((currentState) => [
-                ...currentState,
-                {
-                    // Adding new markers on click
-                    lat: e.latLng.lat(),
-                    lng: e.latLng.lng(),
-                    time: new Date(),
-                },
-            ]),
-        []
-    );
-
-    const mapRef = useRef();
-    const onMapLoad = React.useCallback((map) => {
-        mapRef.current = map;
-    }, []);
 
     const search = (e) => {
         e.preventDefault();
@@ -57,8 +31,7 @@ function App() {
                 setLat(response.data.coord.lat);
                 setLng(response.data.coord.lon);
                 setTemp(response.data.main.temp);
-                setcurrentStatus(response.data.weather[0].main);
-                console.log(response.data.weather[0].main);
+                setcurrentStatus(response.data.weather[0].description);
                 setQuery("");
             })
             .catch(function (error) {
@@ -80,54 +53,19 @@ function App() {
 			{ currentStatus ? 
 			<div>
 	            <p className="top">Temperature: {((temp * 9) / 5 + 32).toFixed(2)}&#8457; </p>
-				<p className="top">Currently: {currentStatus}</p>
+				<p className="top">Currently: {currentStatus.charAt(0).toUpperCase() + currentStatus.slice(1)}</p>
 			</div>
 			:
 			null
 			}
-            <h1 className="h1">Search for the weather by location:</h1>
 
+            <h1 className="h1">Search for the weather by location:</h1>
             <form onSubmit={search}>
-                <input
-                    type="text"
-                    className="search-bar"
-                    placeholder="Search any city"
-                    onChange={(e) => setQuery(e.target.value)}
-                    value={query}
-                />
-                <input type="submit" value="submit" placeholder="submit" />
+                <input type="text" className="form" placeholder="Search Any City" onChange={(e) => setQuery(e.target.value)} value={query} />
+                <input type="submit" className="submit" value="submit" placeholder="submit" />
             </form>
             <br />
-            <GoogleMap
-                mapContainerStyle={mapContainerStyle}
-                zoom={10}
-                center={center}
-                onClick={onMapClick}
-                onLoad={onMapLoad}
-            >
-                {markers.map((x) => (
-                    <Marker
-                        key={x.time.toISOString()}
-                        position={{ lat: x.lat, lng: x.lng }}
-                        onClick={() => setSelected(x)}
-                    />
-                ))}
-
-                {selected ? (
-                    <InfoWindow
-                        position={{ lat: selected.lat, lng: selected.lng }}
-                        onCloseClick={() => setSelected(null)}
-                    >
-                        <div>
-                            <h2>New Location</h2>
-                            <p>
-                                Latitude: {selected.lat}, Longitude:{" "}
-                                {selected.lng}
-                            </p>
-                        </div>
-                    </InfoWindow>
-                ) : null}
-            </GoogleMap>
+            <GoogleMap mapContainerStyle={mapContainerStyle} zoom={10} center={center} />
         </div>
     );
 }
